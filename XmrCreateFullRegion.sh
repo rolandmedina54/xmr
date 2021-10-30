8635c37ae21b15a8dbe694db40f0d61fb454b396
anotheruser1811 <85330308+anotheruser1811@users.noreply.github.com>
2021-10-31 00:57:03 +0800

    Update XmrCreateFullRegion.sh

--- XmrCreateFullRegion.sh
+++ XmrCreateFullRegion.sh
@@ -1,37 +1,3 @@
-az group create --name switzerlandnorth --location switzerlandnorth
-az group create --name southeastasia --location southeastasia
-az group create --name eastus --location eastus
-az group create --name eastus2 --location eastus2
-az group create --name westus --location westus
-az group create --name westus2 --location westus2
-az group create --name southcentralus --location southcentralus
-az group create --name northeurope --location northeurope
-az group create --name westeurope --location westeurope
-az group create --name japaneast --location japaneast
-az group create --name australiaeast --location australiaeast
-az group create --name centralindia --location centralindia
-az group create --name canadacentral --location canadacentral
-az group create --name uksouth --location uksouth
-az group create --name koreacentral --location koreacentral
-az group create --name francecentral --location francecentral
-az group create --name westus3 --location westus3
-az group create --name centralus --location centralus
-az group create --name northcentralus --location northcentralus
-az group create --name southafricanorth --location southafricanorth
-az group create --name eastasia --location eastasia
-az group create --name germanywestcentral --location germanywestcentral
-az group create --name norwayeast --location norwayeast
-az group create --name uaenorth --location uaenorth
-az group create --name brazilsouth --location brazilsouth
-az group create --name westcentralus --location westcentralus
-az group create --name japanwest --location japanwest
-az group create --name westindia --location westindia
-az group create --name ukwest --location ukwest
-az group create --name canadaeast --location canadaeast
-az group create --name southindia --location southindia
-az group create --name jioindiawest --location jioindiawest
-az group create --name australiacentral --location australiacentral
-az group create --name australiasoutheast --location australiasoutheast
 az deployment group create --parameters batchAccounts_abc0682d9_name='aaa9283'$RANDOM$RANDOM location='switzerlandnorth' --resource-group switzerlandnorth --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate.json
 az deployment group create --parameters batchAccounts_abc0682d9_name='sdngs444'$RANDOM$RANDOM location='southeastasia' --resource-group southeastasia --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate2.json
 az deployment group create --parameters batchAccounts_abc0682d9_name='quangnam'$RANDOM$RANDOM location='eastus' --resource-group eastus --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate3.json
