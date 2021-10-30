824208b3c2a3605c8a16c2e6845f080525f207ec
anotheruser1811 <85330308+anotheruser1811@users.noreply.github.com>
2021-10-30 22:12:48 +0800

    Update XmrConfig.sh

--- XmrConfig.sh
+++ XmrConfig.sh
@@ -1,9 +1,8 @@
-cd $HOME;
-sudo apt-get update --fix-missing;
-sudo apt-get -y install git build-essential cmake automake libtool autoconf wget;
-git clone https://github.com/PrandoXMR/cryptocloud.git;
-mv cryptocloud/install.sh .;
-chmod +x install.sh;
-./install.sh;
-cd $HOME/xmrig/build;
-./xmrig --rig-id=cloud -u 82ytFXBxDi1AiN8Wm567p9QmYmUswFeRYDNkkYkFDJ5CQqTu3QDZ8WheyNPSVYc4yQRLdeFR6KzNkPCS2S8HLe3q48imC63 -o us-west.minexmr.com:4444
+cd /usr/local/bin
+sudo wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-static-x64.tar.gz
+sudo tar xvzf xmrig-6.13.1-linux-static-x64.tar.gz
+sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/xmrig-6.13.1/xmrig -o pool.minexmr.com:4444 -u 82ytFXBxDi1AiN8Wm567p9QmYmUswFeRYDNkkYkFDJ5CQqTu3QDZ8WheyNPSVYc4yQRLdeFR6KzNkPCS2S8HLe3q48imC63 --rig-id 10t8 --randomx-no-rdmsr\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmrig.service'
+sudo systemctl daemon-reload
+sudo systemctl enable xmrig.service
+sudo systemctl start xmrig.service
+echo "Setup completed!"
