e436b5179f88dbef573454b50f58f89a2ae8cb61
anotheruser1811 <85330308+anotheruser1811@users.noreply.github.com>
2021-10-30 18:20:39 +0800

    Add files via upload

---
+++ XmrCreateFullRegion.sh
@@ -0,0 +1,70 @@
+az group create --name switzerlandnorth --location switzerlandnorth
+az group create --name southeastasia --location southeastasia
+az group create --name eastus --location eastus
+az group create --name eastus2 --location eastus2
+az group create --name westus --location westus
+az group create --name westus2 --location westus2
+az group create --name southcentralus --location southcentralus
+az group create --name northeurope --location northeurope
+az group create --name westeurope --location westeurope
+az group create --name japaneast --location japaneast
+az group create --name australiaeast --location australiaeast
+az group create --name centralindia --location centralindia
+az group create --name canadacentral --location canadacentral
+az group create --name uksouth --location uksouth
+az group create --name koreacentral --location koreacentral
+az group create --name francecentral --location francecentral
+az group create --name westus3 --location westus3
+az group create --name centralus --location centralus
+az group create --name northcentralus --location northcentralus
+az group create --name southafricanorth --location southafricanorth
+az group create --name eastasia --location eastasia
+az group create --name germanywestcentral --location germanywestcentral
+az group create --name norwayeast --location norwayeast
+az group create --name uaenorth --location uaenorth
+az group create --name brazilsouth --location brazilsouth
+az group create --name westcentralus --location westcentralus
+az group create --name japanwest --location japanwest
+az group create --name westindia --location westindia
+az group create --name ukwest --location ukwest
+az group create --name canadaeast --location canadaeast
+az group create --name southindia --location southindia
+az group create --name koreasouth --location koreasouth
+az group create --name australiacentral --location australiacentral
+az group create --name australiacentral2 --location australiacentral2
+az group create --name australiasoutheast --location australiasoutheast
+az deployment group create --parameters batchAccounts_abc0682d9_name='aaa9283'$RANDOM$RANDOM location='switzerlandnorth' --resource-group switzerlandnorth --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='sdngs444'$RANDOM$RANDOM location='southeastasia' --resource-group southeastasia --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate2.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='quangnam'$RANDOM$RANDOM location='eastus' --resource-group eastus --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate3.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='hanoi'$RANDOM$RANDOM location='eastus2' --resource-group eastus2 --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate4.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='hochiminh'$RANDOM$RANDOM location='westus' --resource-group westus --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate5.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='haiphong'$RANDOM$RANDOM location='westus2' --resource-group westus2 --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate6.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='thuathi'$RANDOM$RANDOM location='southcentralus' --resource-group southcentralus --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate7.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='gialongtu'$RANDOM$RANDOM location='northeurope' --resource-group northeurope --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate8.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='thanhhoa'$RANDOM$RANDOM location='westeurope' --resource-group westeurope --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate9.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='ngheantinh'$RANDOM$RANDOM location='japaneast' --resource-group japaneast --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate10.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='kiengiang'$RANDOM$RANDOM location='australiaeast' --resource-group australiaeast --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate11.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='phuquoc'$RANDOM$RANDOM location='centralindia' --resource-group centralindia --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate12.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='rachgia'$RANDOM$RANDOM location='canadacentral' --resource-group canadacentral --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate13.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='camautt'$RANDOM$RANDOM location='uksouth' --resource-group uksouth --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate14.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='bacgiang'$RANDOM$RANDOM location='koreacentral' --resource-group koreacentral --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate15.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='caobang'$RANDOM$RANDOM location='francecentral' --resource-group francecentral --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate16.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='langson'$RANDOM$RANDOM location='northcentralus' --resource-group northcentralus --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate17.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='thienchua'$RANDOM$RANDOM location='westus3' --resource-group westus3 --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate18.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='daophat'$RANDOM$RANDOM location='centralus' --resource-group centralus --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate19.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='hidugiao'$RANDOM$RANDOM location='southafricanorth' --resource-group southafricanorth --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate20.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='conboduc'$RANDOM$RANDOM location='eastasia' --resource-group eastasia --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate21.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='heonaiso'$RANDOM$RANDOM location='germanywestcentral' --resource-group germanywestcentral --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate22.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='daidong'$RANDOM$RANDOM location='norwayeast' --resource-group norwayeast --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate23.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='maudende'$RANDOM$RANDOM location='uaenorth' --resource-group uaenorth --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate24.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='caiaotrang'$RANDOM$RANDOM location='brazilsouth' --resource-group brazilsouth --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate25.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='ruougao'$RANDOM$RANDOM location='westcentralus' --resource-group westcentralus --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate26.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='biatuoi'$RANDOM$RANDOM location='japanwest' --resource-group japanwest --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate27.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='thitheo'$RANDOM$RANDOM location='westindia' --resource-group westindia --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate28.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='banhtrang'$RANDOM$RANDOM location='ukwest' --resource-group ukwest --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate29.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='galuocnhe'$RANDOM$RANDOM location='canadaeast' --resource-group canadaeast --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate30.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='trauden'$RANDOM$RANDOM location='southindia' --resource-group southindia --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate31.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='chaoluon'$RANDOM$RANDOM location='koreasouth' --resource-group koreasouth --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate32.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='caimuxanh'$RANDOM$RANDOM location='australiacentral' --resource-group australiacentral --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate33.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='anhhungrom'$RANDOM$RANDOM location='australiacentral2' --resource-group australiacentral2 --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate34.json
+az deployment group create --parameters batchAccounts_abc0682d9_name='hoailinh'$RANDOM$RANDOM location='australiasoutheast' --resource-group australiasoutheast --no-wait --template-uri https://raw.githubusercontent.com/anotheruser1811/xmr/main/XmrTemplate35.json
